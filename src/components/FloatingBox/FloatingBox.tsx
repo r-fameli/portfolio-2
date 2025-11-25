8d943126e0ef095d77e0a0983ce297decfea449a
Riki Fameli <114783766+poopycat420@users.noreply.github.com>
2025-11-25 10:36:12 -0500

    Style floating boxes for experience page

--- src/components/FloatingBox/FloatingBox.tsx
+++ src/components/FloatingBox/FloatingBox.tsx
@@ -4,17 +4,17 @@ type Props = {
     title: string,
     subtitle: string,
     datesString: string,
-    content: string,
+    content: React.ReactNode,
 }
 
 const FloatingBox = ({ title, subtitle, datesString, content }: Props) => {
     return <div className="FloatingBox">
         <div className="box-heading-flex">
-            <span>
+            <span className="FloatingBox-titling">
                 <h2>{title}</h2>
-                <h2>{subtitle}</h2>
+                <h2 className="FloatingBox-subtitle">{subtitle}</h2>
             </span>
-            <span>{datesString}</span>
+            <h2>{datesString}</h2>
         </div>
         <div>
             {content}
--- src/pages/Experience/Experience.tsx
+++ src/pages/Experience/Experience.tsx
@@ -1,14 +1,19 @@
 import FloatingBox from "../../components/FloatingBox/FloatingBox";
 
 const msftContent = " Architected and built a full-stack web application (“SKULens”) through a serverless Azure Durable Functions API and a Typescript React static web frontend to migrate away from legacy python scripts and display metrics, charts, and reports related to hardware testing"
-
+const brownTAContent = <ul>
+    <li>Assisted staff in redesigning course website in HTML/CSS/JS/Figma to fit early 2000's theme for the spring 2022 semester</li>
+    <li>Added draggable windows in Javascript and improved ease of access for student resources [Link] [Github]</li>
+    <li>Assisted teaching staff in creating course content and presentations for an introductory course of 200+ undergraduates</li>
+    <li>Taught students foundational concepts of functional programming and data manipulation at lab sessions and teaching hours</li>
+</ul>
 
 const Experience = () => {
     return <div className="page">
         <h1 className="text-center">experience</h1>
         <div className="floating-box-container">
             <FloatingBox title={"Microsoft, Azure Storage"} subtitle={"Software Engineer"} datesString={"Aug 2023 - Sept 2025"} content={msftContent} />
-            <FloatingBox title={"Microsoft, Azure Storage"} subtitle={"Software Engineer"} datesString={"Aug 2023 - Sept 2025"} content={msftContent} />
+            <FloatingBox title={"Brown University Dept. of Computer Science"} subtitle={"Undergraduate Teaching Assistant"} datesString={"Sep 2021 - Dec 2023"} content={brownTAContent} />
         </div>
     </div>
 };
