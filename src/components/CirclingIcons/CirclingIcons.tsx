1d90a61083245e123e0e71d31f6643f82edc1e1a
r-fameli <rikifameli@gmail.com>
2026-01-16 12:25:34 -0800

    Refactor CirclingIcons

--- src/components/CirclingIcons/CirclingIcons.tsx
+++ src/components/CirclingIcons/CirclingIcons.tsx
@@ -2,42 +2,19 @@ import { useState } from 'react';
 import './CirclingIcons.scss';
 import { useAnimationFrame } from '../../hooks/useAnimationFrame';
 
-// Placing Constants, in pixels and degrees
-const CENTER = 150;
-const DIST_TO_CENTER = 350; // Distance to center
-const ROTATION_OFFSET = 10;
-const DIST_X_OFFSET = 20;
-const DIST_Y_OFFSET = -260;
-
 const radians = (degrees: number) => degrees * (Math.PI / 180)
-
-const getCirclePositions = (numCircles: number, offset: number) => {
-    const angleSteps = 360 / numCircles;
-    const positions = new Array(numCircles);
-    const rotationOffsetRadians = radians(ROTATION_OFFSET);
-    for (let i = 0; i < numCircles; i++) {
-        const angle = (offset + angleSteps * i) % 360;
-        
-        const xBeforeRot = (DIST_TO_CENTER + DIST_X_OFFSET) * Math.cos(radians(angle));
-        const yBeforeRot = (DIST_TO_CENTER + DIST_Y_OFFSET) * Math.sin(radians(angle));
-        
-        // Apply rotation to tilt the moons on an axis
-        const cosRot = Math.cos(rotationOffsetRadians);
-        const sinRot = Math.sin(rotationOffsetRadians);
-        const x = CENTER + (xBeforeRot * cosRot - yBeforeRot * sinRot);
-        const y = CENTER + (xBeforeRot * sinRot + yBeforeRot * cosRot);
-        // z-index
-        const z = angle < 180 ? 5 : 0;
-        positions[i] = [x, y, z]
-    };
-    return positions
-}
 
 type Props = {
     numCircles: number,
 }
 
 const CirclingIcons = ({ numCircles }: Props) => {
+    // Placing Constants, in pixels and degrees
+    const CENTER = 150;
+    const DIST_TO_CENTER = 350; // Distance to center
+    const ROTATION_OFFSET = 10;
+    const DIST_X_OFFSET = 20;
+    const DIST_Y_OFFSET = -260;
     // Velocity Variables
     // velocity units are degrees per second
     const VELOCITY_BASE = 15;
@@ -45,7 +22,28 @@ const CirclingIcons = ({ numCircles }: Props) => {
     // STATES
     const [velocity, setVelocity] = useState(VELOCITY_BASE);
     const [angleOffset, setAngleOffset] = useState(0);
-    const circlePositions = getCirclePositions(numCircles, angleOffset)
+
+    const getCirclePositions = (numCircles: number, offset: number) => {
+        const angleSteps = 360 / numCircles;
+        const positions = new Array(numCircles);
+        const rotationOffsetRadians = radians(ROTATION_OFFSET);
+        for (let i = 0; i < numCircles; i++) {
+            const angle = (offset + angleSteps * i) % 360;
+
+            const xBeforeRot = (DIST_TO_CENTER + DIST_X_OFFSET) * Math.cos(radians(angle));
+            const yBeforeRot = (DIST_TO_CENTER + DIST_Y_OFFSET) * Math.sin(radians(angle));
+
+            // Apply rotation to tilt the moons on an axis
+            const cosRot = Math.cos(rotationOffsetRadians);
+            const sinRot = Math.sin(rotationOffsetRadians);
+            const x = CENTER + (xBeforeRot * cosRot - yBeforeRot * sinRot);
+            const y = CENTER + (xBeforeRot * sinRot + yBeforeRot * cosRot);
+            // z-index
+            const z = angle < 180 ? 5 : 0;
+            positions[i] = [x, y, z]
+        };
+        return positions
+    }
 
     useAnimationFrame((deltaTime: number) => {
         if (Math.abs(velocity) > Math.abs(VELOCITY_BASE)) {
@@ -56,6 +54,8 @@ const CirclingIcons = ({ numCircles }: Props) => {
         // Adjust the offset of the circles based on time elapsed
         setAngleOffset(angleOffset => angleOffset + (velocity * deltaTime * 0.001));
     })
+
+    const circlePositions = getCirclePositions(numCircles, angleOffset)
 
     return <div className="CirclingIcons">
         <div className="circling-icons-frame">
