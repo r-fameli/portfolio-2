7fe796177e02d735ebdd43aa1a255c2e0b1258bf
r-fameli <rikifameli@gmail.com>
2026-01-16 15:52:52 -0800

    Get landing page links working

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,5 +1,6 @@
 import CirclingIcons from '../../components/CirclingIcons/CirclingIcons';
 import './Landing.scss'
+import Resume from '../../assets/documents/Resume.pdf';
 
 const Landing = () => {
     return <div className="page" id="landing-page">
@@ -8,10 +9,10 @@ const Landing = () => {
             <div className="text-center title">Riki Fameli</div>
             <div className="text-center subtitle">full stack software engineer</div>
             <div className="landing-list">
-                <div>resume</div>
-                <div>mail</div>
-                <div>github</div>
-                <div>linkedin</div>
+                <div><a href={Resume} target="_blank">resume</a></div>
+                <div><a href="mailto:rikifameli@gmail.com" target="_blank">mail</a></div>
+                <div><a href="https://github.com/r-fameli" target="_blank">github</a></div>
+                <div><a href="https://linkedin.com/in/riki-fameli/" target="_blank">linkedin</a></div>
             </div>
         </div>
     </div>
