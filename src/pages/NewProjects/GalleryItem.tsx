7a44d42ff1f5cad37d07f239c5d5ab055932e6d2
r-fameli <rikifameli@gmail.com>
2026-01-27 12:39:33 -0800

    Update transformOrigin on computed styles for GalleryItems

--- src/pages/NewProjects/GalleryItem.tsx
+++ src/pages/NewProjects/GalleryItem.tsx
@@ -14,17 +14,36 @@ type GalleryItemProps = {
 const GalleryItem = ({ id, source, type, caption, captionSpot = 'topleft', xOffset, yOffset, scale = 1 }: GalleryItemProps) => {
     const captionOnTop = (captionSpot === 'topleft' || captionSpot === 'topright');
     const captionClass = (captionSpot === 'topleft' || captionSpot == 'bottomleft') ? 'left-caption' : 'right-caption';
-
-    const computedStyle = {
-        transform: `translate(${xOffset}%, ${yOffset}%) scale(${scale})`
+    let transformOrigin = null;
+    switch (captionSpot) {
+        case 'bottomleft':
+            transformOrigin = 'bottom left';
+            break;
+        case 'bottomright':
+            transformOrigin = 'bottom right';
+            break;
+        case 'topleft':
+            transformOrigin = 'top left';
+            break;
+        case 'topright':
+            transformOrigin = 'top right';
+            break;
     }
 
+    const computedStyle = {
+        transform: `translate(${xOffset}%, ${yOffset}%) scale(${scale})`,
+        transformOrigin: transformOrigin,
+    }
+
+    const captionStyle = { transform: `translate(${xOffset}%, ${yOffset}%)`, transformOrigin: transformOrigin }
+
+
     return <div className="vertical-item-layout">
-        {captionOnTop && <div className={captionClass} style={{transform: `translate(${xOffset}%, ${yOffset}%)`}}>
+        {captionOnTop && <div className={captionClass} style={captionStyle}>
             {caption}
         </div>}
-        <MediaCard id={id} source={source} type={type} style={computedStyle}/>
-        {!captionOnTop && <div className={captionClass}>
+        <MediaCard id={id} source={source} type={type} style={computedStyle} />
+        {!captionOnTop && <div className={captionClass} style={captionStyle}>
             {caption}
         </div>}
     </div>
